9e77de2d6e820cae7763f3b60ae6c4d10b2dc1f1
gdlstand <gdlstand@163.com>
2022-09-17 22:00:41 +0800

    Delete FAW_PP_scripts_SUB directory

--- FAW_PP_scripts_SUB/001_FAW_PP_Admin.js
+++
@@ -1,5 +0,0 @@
-var user = new Array();
-	user[0] = new Array('FAW_user01', '12345');
-	user[1] = new Array('FAW_user02', '54321');
-	user[2] = new Array('FAW_user03', '11111');
-var user_history = new Array();
